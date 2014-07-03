228cab529499133eaa088a1f5f9108495d790578
amadarain <t.f.octo@gmail.com>
2014-07-03 12:07:28 +0900

    シェーダのソースコードを別のファイルに分離

--- transform_feedback/feedback.cpp
+++ transform_feedback/feedback.cpp
@@ -8,34 +8,9 @@ int main(int argc, char **argv) {
         suika::glfw::initializeWindowAndContext(
         initialSize, initialSize, "transform feedback", nullptr, nullptr, true);
     glfwSetWindowSizeCallback(window, suika::glfw::centeredMaximizedSquareViewport);
+    GLuint particleProgram = 
+        suika::shader::makeProgram("transform_feedback.vert", "transform_feedback.frag");
     
-    std::vector<suika::shader::ShaderSource> particleSources;
-    particleSources.push_back(suika::shader::ShaderSource(GL_VERTEX_SHADER, std::string(
-        "#version 330 core\n"
-        "layout(location=0) in vec2 position;"
-        "layout(location=1) in float color;"
-        "out vec2 newPosition;"
-        "out float vfColor;"
-        "float step = 0.005f;"
-        "void main(void) {"
-        "   vec2 normal = normalize(position);"
-        "   vec2 pos = position + step * normal;"
-        "   if (pos.x > 1.0f || pos.x < -1.0f"
-        "       || pos.y > 1.0f || pos.y < -1.0f) {"
-        "       pos = step * normalize(pos);"
-        "   }"
-        "   newPosition = pos;"
-        "   gl_Position = vec4(position, 0.0f, 1.0f);"
-        "   vfColor = color;"
-        "}"), "particle vertex shader"));
-    particleSources.push_back(suika::shader::ShaderSource(GL_FRAGMENT_SHADER, std::string(
-        "#version 330 core\n"
-        "layout(location=0) out vec4 color;"
-        "in float vfColor;"
-        "void main(void) {"
-        "   color = vec4(0.8f, 0.8f, vfColor, 1.0f);"
-        "}"), "particle fragment shader"));
-    GLuint particleProgram = suika::shader::createShaderProgram(particleSources);
     glUseProgram(particleProgram);
     GLuint particleVAO;
     glGenVertexArrays(1, &particleVAO);
